9eb809b0ebc20b5cf11f58eae23e57886d6f0c71
yangtao3163 <316347421@qq.com>
2017-07-19 19:34:24 +0800

    文章管理分类部分

--- backend/views/article/add.php
+++ backend/views/article/add.php
@@ -1,9 +1,11 @@
 <?php
 $form = \yii\bootstrap\ActiveForm::begin();//表单开始
 echo $form->field($model,"name")->textInput();
-echo $form->field($model,"intro")->textarea();
-echo $form->field($model,"article_category_id")->textInput();
+echo  $form->field($model,"article_category_id")->dropDownList(\backend\models\Article::getAuthorOptions());
 echo $form->field($model,"sort")->textInput(['type'=>'number']);
 echo $form->field($model,"status",['inline'=>true])->radioList(\backend\models\Brand::getStatusOptions());
+echo $form->field($model,"intro")->textarea();
+echo $form->field($model2,"content")->textarea(['rows'=>10]);
+
 echo \yii\bootstrap\Html::submitButton('提交',['class'=>'btn btn-info']);
 \yii\bootstrap\ActiveForm::end();//表单结束
